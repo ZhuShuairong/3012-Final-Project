2b3ed1aafd61da59653c246f6220eba144603ee5
Dorothy131313 <163135161+Dorothy131313@users.noreply.github.com>
2024-04-28 11:32:25 +0800

    Add files via upload

--- login-info.sql
+++ login-info.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2024 at 12:57 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Apr 28, 2024 at 05:30 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `focus-user-data`
+-- Database: `mydata`
 --
 
 -- --------------------------------------------------------
@@ -34,15 +34,18 @@ CREATE TABLE `login-info` (
   `password` varchar(12) NOT NULL,
   `coins` int(8) NOT NULL DEFAULT 20,
   `inventory` varchar(1000) NOT NULL,
-  `settings` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `settings` varchar(500) NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `login-info`
 --
 
-INSERT INTO `login-info` (`userid`, `username`, `email`, `password`, `coins`, `inventory`, `settings`) VALUES
-(1, 'admin', 'admin@tony.com', '3012', 20, '', '');
+INSERT INTO `login-info` (`userid`, `username`, `email`, `password`, `coins`, `inventory`, `settings`, `last_update`) VALUES
+(1, 'admin', 'admin@tony.com', '3012', 15, '', '', '2024-04-26 15:20:19'),
+(5, 'zxz', '1223', '200211', 45, '1;3', '', '2024-04-28 03:23:52'),
+(6, 'moon', '250', '111111', 20, '', '', '2024-04-26 15:20:19');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +65,7 @@ ALTER TABLE `login-info`
 -- AUTO_INCREMENT for table `login-info`
 --
 ALTER TABLE `login-info`
-  MODIFY `userid` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `userid` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
